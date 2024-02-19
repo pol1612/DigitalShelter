1deeb9a7e36b918561685f0608e946c119490dd3
Pol Sañé Jové <pol.sane@uvic.cat>
2024-02-19 15:17:08 +0000

    added root navgraph and the auth and main graph, with the screens without being implemented of the auth graph

---
+++ app/src/main/java/com/pol/sane/jove/digitalshelter/graphs/MainNavGraph.kt
@@ -0,0 +1,9 @@
+package com.pol.sane.jove.digitalshelter.graphs
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+
+fun NavGraphBuilder.mainNavGraph(
+    navController: NavHostController) {
+    TODO("Not yet implemented")
+}
---
+++ app/src/main/java/com/pol/sane/jove/digitalshelter/screens/auth/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.pol.sane.jove.digitalshelter.screens.auth
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen(){
+
+}
